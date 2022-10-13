1241b28d84cd4bbd2c123c739a0c9c4911841bc8
Nathan <nathan_ramirez19@yahoo.com>
2022-10-12 19:03:32 -0700

    new

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World');
+    res.send(`Hello, ${process.env.NAME}`);
 });
 
 app.listen(PORT, () => {console.log('App is running')})
